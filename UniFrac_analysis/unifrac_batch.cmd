eddd3d68a2a03ccbb668a2e8b2d649c0a7509ed1
alexlinz <amlinz@wisc.edu>
2016-03-31 15:44:28 -0500

    Expanded UniFrac analysis to include all lakes and years; will look into replacing the Bray-Curtis similarity analyses with UniFrac

--- UniFrac_analysis/unifrac_batch.cmd
+++ UniFrac_analysis/unifrac_batch.cmd
@@ -6,10 +6,15 @@ SET me=%~n0
 SET parent=%~dp0
 
 ::Run script to make count file and parse fasta file to remove sequences with no reads in these samples
-"C:\Program Files\R\R-3.0.2\bin\x64\Rscript.exe" C:\Users\amlinz16\Desktop\North_Temperate_Lakes-Microbial_Observatory\UniFrac_analysis\02OTUtable_to_countfile.R %1 %2 %3
+"C:\Program Files\R\R-3.2.4revised\bin\x64\Rscript.exe" C:\Users\amlinz16\Desktop\North_Temperate_Lakes-Microbial_Observatory\UniFrac_analysis\02OTUtable_to_countfile.R %1 %2 %3
 
-::Test to see if I can run mothur commands in the command prompt
+::Run mothur commands
 C:\Users\amlinz16\Desktop\Programs\Mothur.win_64\mothur\mothur.exe C:\Users\amlinz16\Desktop\North_Temperate_Lakes-Microbial_Observatory\UniFrac_analysis\01unifrac_mothur_commands.txt
 
+::Rename output
+ren temp.trewsummary %1-%2-%3.trewsummary
+
+::Get average UniFrac distance for each date in the mixed lake
+"C:\Program Files\R\R-3.2.4revised\bin\x64\Rscript.exe" C:\Users\amlinz16\Desktop\North_Temperate_Lakes-Microbial_Observatory\UniFrac_analysis\03parse_unifrac_output.R %1 %2 %3
 
 @EXIT /B 0
